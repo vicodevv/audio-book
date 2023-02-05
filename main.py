3a1de7b1e3f87dde82584168ce417e8a58b19dcf
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-02-05 11:52:10 +0100

    Feature update: Installed Python Packages

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import PyPDF2
 import pyttsx3
+
+book = open("CTCI.pdf", "rb").read()
+pdfReader = PyPDF2.PdfFileReader(book)
 
 engine = pyttsx3.init()
 
-engine.say("Hello World")
+engine.say(book)
 engine.runAndWait()
